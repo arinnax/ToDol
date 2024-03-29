96308f864fd6bdada2a02252a041b89e5cf1d133
arinnax <163285407+arinnax@users.noreply.github.com>
2024-03-29 14:13:43 +0300

    Add files via upload

---
+++ Ту-ду лист готовый.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace To_do_list
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.taskTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.taskListBox = new System.Windows.Forms.CheckedListBox();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.titleLabel.Font = new System.Drawing.Font("Rotonda Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.titleLabel.ForeColor = System.Drawing.Color.Black;
+            this.titleLabel.Location = new System.Drawing.Point(189, 90);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(112, 22);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Список дел";
+            this.titleLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // taskTextBox
+            // 
+            this.taskTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.taskTextBox.Location = new System.Drawing.Point(193, 152);
+            this.taskTextBox.Name = "taskTextBox";
+            this.taskTextBox.Size = new System.Drawing.Size(100, 20);
+            this.taskTextBox.TabIndex = 1;
+            // 
+            // addButton
+            // 
+            this.addButton.BackColor = System.Drawing.Color.White;
+            this.addButton.Location = new System.Drawing.Point(143, 235);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(98, 39);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // taskListBox
+            // 
+            this.taskListBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.taskListBox.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.taskListBox.FormattingEnabled = true;
+            this.taskListBox.Location = new System.Drawing.Point(456, 90);
+            this.taskListBox.Name = "taskListBox";
+            this.taskListBox.Size = new System.Drawing.Size(228, 184);
+            this.taskListBox.TabIndex = 3;
+            this.taskListBox.SelectedIndexChanged += new System.EventHandler(this.taskListBox_SelectedIndexChanged);
+            // 
+            // removeButton
+            // 
+            this.removeButton.BackColor = System.Drawing.Color.White;
+            this.removeButton.Location = new System.Drawing.Point(276, 235);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(98, 39);
+            this.removeButton.TabIndex = 4;
+            this.removeButton.Text = "Удалить";
+            this.removeButton.UseVisualStyleBackColor = false;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Location = new System.Drawing.Point(456, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 33);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.taskListBox);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.taskTextBox);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TextBox taskTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.CheckedListBox taskListBox;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Ту-ду лист готовый.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace To_do_list
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string task = taskTextBox.Text;
+            if
+                (!string.IsNullOrWhiteSpace(task)) //Если строка не нулевая или не только с пробелами - задача добавляется в лист, поле очищается
+            {
+                taskListBox.Items.Add(task);
+                taskTextBox.Clear();
+            }
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if (taskListBox.SelectedIndex != -1) //Для удаления нужно выбрать удаляеме действие
+            {
+                string selectedTask = taskListBox.SelectedItem?.ToString();  // Добавлена проверка на null
+                if (!string.IsNullOrEmpty(selectedTask))
+                {
+                    taskListBox.Items.Remove(selectedTask); //Удаляем
+
+
+                }
+            }
+        }
+
+        private void SaveTasksToFile()
+        {
+            string filePath = "tasks.txt"; // Путь к файлу, в который будем сохранять задачи
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (string task in taskListBox.Items)
+                {
+                    writer.WriteLine(task); // Записываем каждую задачу в новую строку файла
+                }
+            }
+
+            MessageBox.Show("Список задач сохранен в файл.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveTasksToFile();
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void taskListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
